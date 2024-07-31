5624b3735e2ee9d3d556f27316f3df64c2a9d7d3
paulpierce34 <33561650+paulpierce34@users.noreply.github.com>
2024-07-31 11:20:14 -0400

    Add files via upload

---
+++ Habbo-AutoTyper.ps1
@@ -0,0 +1,18 @@
+﻿## Simple powershell script to auto-type for you in Habbo Origins (or other applications)
+
+$SpeechText = "buying bird bath -----> 8 teal ff pods and 3 tubs" ## Set this value to the string you'd like to type
+$SleepTimer = 2 ## Set this value to how long to sleep for
+$ApplicationName = "Habbo Hotel: Origins" ## Habbo process name
+
+
+## Don't touch this section ############################
+while ($True){
+
+$wshell = New-Object -ComObject wscript.shell;
+$wshell.AppActivate($ApplicationName)
+$wshell.SendKeys($SpeechText)
+$wshell.SendKeys('{ENTER}')
+Sleep $SleepTimer
+write-host -foregroundcolor Yellow "Sleeping for $SleepTimer seconds.."
+}
+############################
